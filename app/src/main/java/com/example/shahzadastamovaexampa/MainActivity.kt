03da2e875f86a4169d50853f688c2f5f830d72b3
imshahastam <shahzada.stamova@gmail.com>
2021-12-04 20:30:28 +0600

    add info contact

--- app/src/main/java/com/example/shahzadastamovaexampa/MainActivity.kt
+++ app/src/main/java/com/example/shahzadastamovaexampa/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
 
+//Shahzada Stamova
 class MainActivity : AppCompatActivity(), EnterData {
 
     override fun onCreate(savedInstanceState: Bundle?) {
